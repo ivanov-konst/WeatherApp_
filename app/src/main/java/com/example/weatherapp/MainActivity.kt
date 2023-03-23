fd59ce1ea9fda40c8b3d8e04813ddf183a3dbc61
Konstantin <ivanov-konst@list>
2023-03-23 21:46:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.weatherapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.tvHellow)
+        textViewHello.text = "Hellow from code!"
+    }
+}/
